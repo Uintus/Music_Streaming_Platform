3b459d6f59c5a922d2b87380f83f11208b740800
UinTus <tuuyen294@gmail.com>
2024-05-11 00:13:27 +0700

    add file

---
+++ src/main/java/com/Se2/MusicPlatform/controller/MusicController.java
@@ -0,0 +1,20 @@
+package com.Se2.MusicPlatform.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class MusicController {
+    @RequestMapping(value = "/navigate")
+    public String getNavigationBar() {
+        System.out.println("navigate okk");
+        return "components/NavigateBar";
+
+    }
+}
---
+++ src/test/java/com/Se2/MusicPlatform/MusicPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Se2.MusicPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
