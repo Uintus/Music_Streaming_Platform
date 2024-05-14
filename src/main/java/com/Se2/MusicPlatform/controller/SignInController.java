c5f29715c6e4b899c4b6bde2f65418be42d961b9
pinkydapoet <127115988+pinkydapoet@users.noreply.github.com>
2024-05-14 14:59:04 +0700

    Update SignInController.java

--- src/main/java/com/Se2/MusicPlatform/controller/SignInController.java
+++ src/main/java/com/Se2/MusicPlatform/controller/SignInController.java
@@ -1,4 +1,4 @@
-'package com.Se2.MusicPlatform.controller;
+package com.Se2.MusicPlatform.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
