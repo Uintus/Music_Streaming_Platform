b21264f4db764e9e56e48b646d6dfa98cc74a1ac
pinkydapoet <127115988+pinkydapoet@users.noreply.github.com>
2024-05-14 14:28:34 +0700

    Add files via upload

---
+++ src/main/java/com/Se2/MusicPlatform/service/UserDetailsService.java
@@ -0,0 +1,26 @@
+package SE2.MusicWebApp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import SE2.MusicWebApp.model.User;
+import SE2.MusicWebApp.repository.UserRepository;
+
+@Service
+public class UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
+}
