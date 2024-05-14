fe49dacef52fe2a3305825014c096bfb3125d2c3
pinkydapoet <127115988+pinkydapoet@users.noreply.github.com>
2024-05-14 15:50:39 +0700

    Update Account.java

--- src/main/java/com/Se2/MusicPlatform/model/Account.java
+++ src/main/java/com/Se2/MusicPlatform/model/Account.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     private String name;
     private String password;
@@ -20,11 +20,11 @@ public class Account {
     @ManyToOne
     private User user;
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
