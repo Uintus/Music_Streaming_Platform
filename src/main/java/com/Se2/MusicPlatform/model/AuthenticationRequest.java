c3f82141754ed12e9277ba75e626f8b0b155529d
pinkydapoet <127115988+pinkydapoet@users.noreply.github.com>
2024-05-14 14:26:17 +0700

    Add files via upload

---
+++ src/main/java/com/Se2/MusicPlatform/model/AuthenticationRequest.java
@@ -0,0 +1,21 @@
+package SE2.MusicWebApp.model;
+
+public class AuthenticationRequest {
+    
+    private String username;
+    private String password;
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
