af2125bea5f1ad689b47ad19f074fa48c9ff1377
Yankery <trang2003bh@gmail.com>
2024-05-14 16:37:01 +0700

    remove all authen process

--- src/main/java/com/Se2/MusicPlatform/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Se2.MusicPlatform.repository;
-
-import com.Se2.MusicPlatform.model.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
