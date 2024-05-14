a1801935d8df759030e9da8111b54a9eb72654a3
pinkydapoet <127115988+pinkydapoet@users.noreply.github.com>
2024-05-14 15:52:26 +0700

    Add files via upload

---
+++ src/main/java/com/Se2/MusicPlatform/repository/AccountRepository.java
@@ -0,0 +1,7 @@
+package com.Se2.MusicPlatform.repository;
+
+import com.Se2.MusicPlatform.model.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
