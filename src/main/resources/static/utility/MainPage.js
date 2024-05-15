044d9a8b4adba7a9af049981e503c3489cc54001
Yankery <trang2003bh@gmail.com>
2024-05-15 10:30:25 +0700

    song detail become dynamic

--- src/main/resources/static/utility/MainPage.js
+++ src/main/resources/static/utility/MainPage.js
@@ -42,9 +42,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
   lyrics.addEventListener("click", function (event) {
       event.preventDefault();
+      let currentlyPlayingSongId = 0;
+      currentlyPlayingSongId = $("#play-image").attr("alt");
 
           // Sử dụng fetch() để tải nội dung từ tệp HTML riêng biệt
-          fetch('/lyrics')
+          fetch('/song_detail/' + currentlyPlayingSongId)
               .then(response => response.text())
               .then(data => {
                   contentElement.innerHTML = data; // Gán nội dung vào phần tử gốc
