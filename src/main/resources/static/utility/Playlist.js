9fe0cb458f460517f8c35a60ca61608ebdf41570
Yankery <trang2003bh@gmail.com>
2024-05-16 11:26:27 +0700

    popup exist

--- src/main/resources/static/utility/Playlist.js
+++ src/main/resources/static/utility/Playlist.js
@@ -1,18 +1,9 @@
-//let addPlaylist = document.getElementById('addPlaylist');
-//let bg = document.getElementsByClassName("main")[0];
-//[0];
-//let popup = document.getElementsByClassName("popup-container")[0];
-//[0];
-//
-//addPlaylist.addEventListener("click", () => {
-//  popup.style.display = "flex";
-//});
-//
-//document.getElementById("closeButton").addEventListener("click", function () {
-//  // Ẩn thẻ popup-container khi nhấn nút "X"
-//  popup.style.display = "none";
-//});
-
+function usePopup() {
+  $("#popup-container").css("display", "flex");
+};
+function hidePopup() {
+  $("#popup-container").css("display", "none");
+};
     function deleteSong(songId) {
       fetch(`/song/delete/${songId}`, {
         method: 'GET', // Change the request method to GET
